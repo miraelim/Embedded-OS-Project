6bd483e9a282032a813d1fccf80663f50a8a689c
miraelim <lovemirae94@gmail.com>
2022-08-15 23:11:11 +0900

    5.4 implement vsprintf() (lib/stdio.c)

--- ch5/lib/stdio.c
+++ ch5/lib/stdio.c
@@ -2,6 +2,9 @@
 #include "HalUart.h"
 #include "stdio.h"
 
+#define PRINTF_BUF_LEN  1024
+
+static char printf_buf[PRINTF_BUF_LEN];   // 1KB
 
 uint32_t putstr(const char* s)
 {
@@ -22,4 +25,61 @@ uint32_t debug_printf(const char* format, ...)
 	va_end(args);
 
 	return putstr(printf_buf);
+}
+
+uint32_t vsprintf(char* buf, const char* format, va_list arg)
+{
+    uint32_t c = 0;
+
+    char     ch;
+    char* str;
+    uint32_t uint;
+    uint32_t hex;
+
+    for (uint32_t i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+            case 'c':
+                ch = (char)va_arg(arg, int32_t);
+                buf[c++] = ch;
+                break;
+            case 's':
+                str = (char*)va_arg(arg, char*);
+                if (str == NULL)
+                {
+                    str = "(null)";
+                }
+                while (*str)
+                {
+                    buf[c++] = (*str++);
+                }
+                break;
+            case 'u':
+                uint = (uint32_t)va_arg(arg, uint32_t);
+                c += utoa(&buf[c], uint, utoa_dec);
+                break;
+            case 'x':
+                hex = (uint32_t)va_arg(arg, uint32_t);
+                c += utoa(&buf[c], hex, utoa_hex);
+                break;
+            }
+        }
+        else
+        {
+            buf[c++] = format[i];
+        }
+    }
+
+    if (c >= PRINTF_BUF_LEN)
+    {
+        buf[0] = '\0';
+        return 0;
+    }
+
+    buf[c] = '\0';
+    return c;
 }
