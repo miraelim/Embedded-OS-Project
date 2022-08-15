e4a5cd709bf835e7fe1257703873e24e5a918889
miraelim <lovemirae94@gmail.com>
2022-08-15 22:24:49 +0900

    5.4 define debug_printf() function (lib/stdio.h)

--- ch5/lib/stdio.h
+++ ch5/lib/stdio.h
@@ -1,13 +1,6 @@
-/*
- * stdio.h
- *
- *  Created on: Sep 17, 2018
- *      Author: maanu
- */
-
 #ifndef LIB_STDIO_H_
 #define LIB_STDIO_H_
 
 uint32_t putstr(const char* s);
-
+uint32_t debug_printf(const char* format, ...);
 #endif /* LIB_STDIO_H_ */
