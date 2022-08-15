4b8e0d57bc1d9b3298f03b1f34894861becb090f
miraelim <lovemirae94@gmail.com>
2022-08-15 22:36:32 +0900

    5.4 implement debug_printf() (lib/stdio.c)

--- ch5/lib/stdio.c
+++ ch5/lib/stdio.c
@@ -13,3 +13,13 @@ uint32_t putstr(const char* s)
     }
     return c;
 }
+
+uint32_t debug_printf(const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+	vsprintf(printf_buf, format, args);
+	va_end(args);
+
+	return putstr(printf_buf);
+}
